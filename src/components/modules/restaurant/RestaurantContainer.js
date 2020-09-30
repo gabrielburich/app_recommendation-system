906bdcc1edff33dbffe41a3e12d82c827e364879
gburich <gabriel.burich@suntech.com.br>
2020-09-30 20:00:18 -0300

    [Restaurant] - Lista de pratos de um restaurant

--- src/components/modules/restaurant/RestaurantContainer.js
+++ src/components/modules/restaurant/RestaurantContainer.js
@@ -1,15 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {getData} from "../../../api/api";
-import {RECOMMEND_URL} from "../../../api/api-url-consts";
-import {showErrorNotification} from "@utils/notifications";
-import {Col, Row} from "antd";
+import React, { useEffect, useState } from 'react';
+import { getData } from '../../../api/api';
+import { RECOMMEND_URL } from '../../../api/api-url-consts';
+import { showErrorNotification } from '@utils/notifications';
+import { Col, Row } from 'antd';
 
-import RestaurantList from "./RestaurantList";
-import RestaurantDetail from "./RestaurantDetail";
-import RestaurantFilter from "./RestaurantFilter";
+import RestaurantList from './RestaurantList';
+import RestaurantDetail from './RestaurantDetail';
+import RestaurantFilter from './RestaurantFilter';
 
 const RestaurantContainer = () => {
-
     const [LIST, DETAIL] = ['LIST', 'DETAIL'];
 
     const initialFilter = {
@@ -20,36 +19,38 @@ const RestaurantContainer = () => {
         distance: 15,
         typeOrderId: 1,
         sitPlace: false,
-    }
+    };
 
     const [dataset, setDataset] = useState([]);
     const [detailItem, setDetailItem] = useState([]);
     const [visualization, setVisualization] = useState(LIST);
 
-    const toggleVisualization = () => setVisualization(visualization => visualization === LIST ? DETAIL : LIST);
+    const toggleVisualization = () => setVisualization((visualization) => (visualization === LIST ? DETAIL : LIST));
 
     const parseFilter = (filter) => {
-        const {pageSize, acceptsMealVoucher, typeRestaurantId, orderTime, distance, typeOrderId, sitPlace} = filter;
-        return `?params=[${pageSize},${acceptsMealVoucher},${typeRestaurantId},${orderTime},${distance},${typeOrderId},${sitPlace}]`
-    }
+        const { pageSize, acceptsMealVoucher, typeRestaurantId, orderTime, distance, typeOrderId, sitPlace } = filter;
+        return `?params=[${pageSize},${acceptsMealVoucher},${typeRestaurantId},${orderTime},${distance},${typeOrderId},${sitPlace}]`;
+    };
 
     const getDataset = (filter = initialFilter) => {
         setVisualization(LIST);
         getData(RECOMMEND_URL + parseFilter(filter))
-            .then(result => setDataset(result.data))
-            .catch(error => {
+            .then((result) => setDataset(result.data))
+            .catch((error) => {
                 console.error(error);
                 showErrorNotification('Error on Load Restaurants');
             });
-    }
+    };
 
     const handleDetail = (item) => {
         setDetailItem(item);
-        toggleVisualization()
+        toggleVisualization();
     };
 
+    const handleBuy = (restaurantId, meal) => {};
+
     useEffect(() => {
-        getDataset()
+        getDataset();
     }, []); //eslint-disable-line
 
     return (
@@ -58,15 +59,14 @@ const RestaurantContainer = () => {
                 <RestaurantFilter initialFilter={initialFilter} onFilter={getDataset} />
             </Col>
             <Col span={18}>
-                {
-                    (visualization === LIST)
-                        ? <RestaurantList dataset={dataset} handleDetail={handleDetail}/>
-                        : <RestaurantDetail item={detailItem} handleBack={toggleVisualization}/>
-                }
+                {visualization === LIST ? (
+                    <RestaurantList dataset={dataset} handleDetail={handleDetail} />
+                ) : (
+                    <RestaurantDetail item={detailItem} handleBack={toggleVisualization} handleBuy={handleBuy} />
+                )}
             </Col>
         </Row>
-
-    )
+    );
 };
 
 export default RestaurantContainer;
--- src/components/modules/restaurant/RestaurantDetail.js
+++ src/components/modules/restaurant/RestaurantDetail.js
@@ -1,20 +1,65 @@
-import React from "react";
-import {Col, PageHeader, Row} from "antd";
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Divider, Descriptions, List, Modal, PageHeader, Row } from 'antd';
+import { getData } from '../../../api/api';
+import { RESTAURANT_URL } from '../../../api/api-url-consts';
+import { showErrorNotification } from '../../../util/notifications';
 
-const RestaurantDetail = ({item, handleBack}) => {
+const ListItem = List.Item;
+const ItemMeta = List.Item.Meta;
+const DescriptionsItem = Descriptions.Item;
+
+const RestaurantDetail = ({ item, handleBack, handleBuy }) => {
+    const [restaurant, setRestaurant] = useState();
+
+    useEffect(() => {
+        getData(RESTAURANT_URL + item.id)
+            .then((result) => setRestaurant(result.data))
+            .catch((error) => {
+                console.error(error);
+                showErrorNotification('Error on Load Restaurant');
+            });
+    }, []); //eslint-disable-line
+
+    const handleConfirmBuy = (meal) => {
+        Modal.confirm({
+            title: 'Are you sure you want to buy the product?',
+            content: (
+                <Descriptions title={item.name} size={'small'} column={1} bordered={true}>
+                    <DescriptionsItem label={'Name'}>{meal.name}</DescriptionsItem>
+                    <DescriptionsItem label={'Description'}>{meal.description}</DescriptionsItem>
+                    <DescriptionsItem label={'Price'}>{meal.price}</DescriptionsItem>
+                </Descriptions>
+            ),
+            onOk: () => {
+                handleBuy && handleBuy(item.id, meal);
+            },
+        });
+    };
+
+    const renderItem = (item) => (
+        <ListItem
+            actions={[
+                <Button type={'link'} onClick={() => handleConfirmBuy(item)}>
+                    Buy
+                </Button>,
+            ]}
+            extra={`$${item.price}`}
+        >
+            <ItemMeta title={item.name} description={item.description} />
+        </ListItem>
+    );
 
     return (
         <Row>
             <Col span={24}>
-                <PageHeader
-                    title={item.name}
-                    subTitle={item.type}
-                    onBack={handleBack}
-                />
+                <PageHeader title={item.name} subTitle={item.type} onBack={handleBack} />
+            </Col>
+            <Col span={24}>
+                <Divider />
+                {restaurant && <List className={'app-list'} dataSource={restaurant.meals} renderItem={renderItem} />}
             </Col>
         </Row>
-    )
-
+    );
 };
 
 export default RestaurantDetail;
