b03a15e5d56f173248701a2eae055493ad1479ef
gburich <gabriel.burich@suntech.com.br>
2020-10-01 17:52:25 -0300

    [OrderModel] - Trazendo pedidos de um usuário ao logar

--- src/store/models/orderModel.js
+++ src/store/models/orderModel.js
@@ -2,12 +2,17 @@ import { action, thunk, thunkOn } from 'easy-peasy';
 import { postData } from '@api/api';
 import { ORDER_URL } from '@api/api-url-consts';
 import { showSuccessNotification, showErrorNotification } from '@utils/notifications';
+import { getData } from '../../api/api';
 
 export const orderModel = {
     orders: [],
 
     addOrder: action((state, payload) => {
         state.orders.push(payload);
+    }),
+
+    setOrders: action((state, payload) => {
+        state.orders = payload;
     }),
 
     saveOrder: thunk((actions, payload) => {
@@ -19,10 +24,26 @@ export const orderModel = {
             });
     }),
 
+    fetchOrders: thunk((actions) => {
+        getData(ORDER_URL)
+            .then((result) => actions.setOrders(result.data))
+            .catch((error) => {
+                console.error(error);
+                showErrorNotification('Error on Load Orders');
+            });
+    }),
+
     onAddOrder: thunkOn(
         (actions) => actions.addOrder,
         (actions, target) => {
             actions.saveOrder(target.payload);
+        }
+    ),
+
+    onLogin: thunkOn(
+        (actions, storeActions) => storeActions.loginModel.addToken,
+        (actions) => {
+            actions.fetchOrders();
         }
     ),
 };
